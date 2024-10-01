cd85252b210d0e58881dbba9bf9379caf9e4edef
Inseok Kang <combikms@naver.com>
2024-10-01 19:02:47 +0900

    :sparkles: Feat: 로컬이미지 업로드 기능

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import './style.dart' as style;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/rendering.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 void main() {
   runApp(
@@ -24,6 +26,7 @@ class _MyAppState extends State<MyApp> {
   var tab = 0;
   var post = [];
   var scroll = ScrollController();
+  var userImage;
 
   getData() async {
     var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
@@ -52,7 +55,23 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: Text('Jjapstagram'),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.add_box_outlined))
+          IconButton(onPressed: () async {
+            var picker = ImagePicker();
+            var image = await picker.pickImage(source: ImageSource.gallery);
+            if (image != null) {
+              setState(() {
+                userImage = File(image.path);
+              });
+            }
+
+            Navigator.push(context,
+              MaterialPageRoute(builder: (c) => Upload(userImage: userImage, addPost: (newPost) {
+                setState(() {
+                  post = [newPost, ...post];
+                });
+              }))
+            );
+          }, icon: Icon(Icons.add_box_outlined))
         ],
       ),
       body: [
@@ -86,7 +105,7 @@ class Post extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.network(post['image'].toString()),
+          post['image'] is String ? Image.network(post['image'].toString()) : Image.file(post['image']),
           Container(
             padding: EdgeInsets.all(15),
             child: Column(
@@ -101,5 +120,41 @@ class Post extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class Upload extends StatelessWidget {
+  Upload({Key? key, this.userImage, this.addPost}) : super(key: key);
+  final userImage;
+  final addPost;
+  var content = TextEditingController();
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar( actions: [
+          IconButton(onPressed: (){
+            var newPost = {
+              'id': 100,
+              'image': userImage,
+              "likes": 30,
+              "date": "Sept 30",
+              "content": content.text,
+              "liked": false,
+              "user": 'Kang',
+            };
+            addPost(newPost);
+            Navigator.pop(context);
+          }, icon: Icon(Icons.send))
+        ],),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.file(userImage),
+            TextField(controller: content,),
+          ],
+        )
+    );
+
   }
 }
